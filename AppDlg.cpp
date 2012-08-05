4c0e4ffea57e04f55db5ea8a2a5696bad60ec243
Admin <admin@chrisoldwood.com>
2012-08-05 20:15:32 +0000

    Added inital view for the temporary fixed WMI properties.

--- AppDlg.cpp
+++ AppDlg.cpp
@@ -8,22 +8,29 @@
 #include "Resource.h"
 #include "FarmMonitor.hpp"
 #include <WMI/Connection.hpp>
-#include <WMI/Win32_OperatingSystem.hpp>
 #include <WMI/Exception.hpp>
+#include <WMI/Win32_OperatingSystem.hpp>
+#include <WMI/Win32_LogicalDisk.hpp>
+#include <WCL/BusyCursor.hpp>
 
 ////////////////////////////////////////////////////////////////////////////////
 //! Constructor.
 
-AppDlg::AppDlg()
+AppDlg::AppDlg(HostsPtr hosts)
 	: CMainDlg(IDD_MAIN)
+	, m_hosts(hosts)
 {
 	DEFINE_CTRL_TABLE
-		CTRL(IDC_HOSTS,	&m_hosts)
+		CTRL(IDC_HOSTS,	&m_hostView)
 	END_CTRL_TABLE
 
 	DEFINE_GRAVITY_TABLE
 		CTRLGRAV(IDC_HOSTS, LEFT_EDGE, TOP_EDGE, RIGHT_EDGE, BOTTOM_EDGE)
 	END_GRAVITY_TABLE
+
+	DEFINE_CTRLMSG_TABLE
+		NFY_CTRLMSG(IDC_HOSTS, LVN_ITEMCHANGED, &AppDlg::onHostSelectionChanged)
+	END_CTRLMSG_TABLE
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -34,32 +41,118 @@ AppDlg::~AppDlg()
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+//! Is a host currently selected?
+
+bool AppDlg::isHostSelected() const
+{
+	return m_hostView.IsSelection();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//! Add a new host to be monitored.
+
+void AppDlg::addHost(const tstring& hostname)
+{
+	CBusyCursor busyCursor;
+
+	size_t index = m_hosts->addHost(hostname);
+
+	addHostToView(index);
+	refreshHost(index);
+
+	if (!m_hostView.IsSelection())
+		m_hostView.Select(0);
+}
+	
+////////////////////////////////////////////////////////////////////////////////
+//! Remove the currently selected host.
+
+void AppDlg::removeSelectedHost()
+{
+	ASSERT(m_hostView.IsSelection());
+
+	const size_t selection = m_hostView.Selection();
+
+	m_hosts->removeHost(selection);
+	m_hostView.DeleteItem(selection);
+
+	if (m_hostView.ItemCount() != 0)
+	{
+		const size_t newSelection = (selection == 0) ? 0 : selection-1;
+
+		m_hostView.Select(newSelection);
+	}
+}
+	
+////////////////////////////////////////////////////////////////////////////////
 //! Handle dialog creation.
 
 void AppDlg::OnInitDialog()
 {
-	initialiseHostsView();
-
+	initialiseHostView();
 	refreshView();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//! Handle change in host selection.
+
+LRESULT AppDlg::onHostSelectionChanged(NMHDR& header)
+{
+	NMLISTVIEW& message = reinterpret_cast<NMLISTVIEW&>(header);
+
+	if (message.uChanged & LVIF_STATE)
+		g_app.m_appCmds.UpdateUI();
+
+	return 0;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 //! Initialise the hosts view.
 
-void AppDlg::initialiseHostsView()
+void AppDlg::initialiseHostView()
 {
-	const FarmMonitor::HostList& hosts = g_app.m_hostList;
+	m_hostView.FullRowSelect(true);
 
-	m_hosts.InsertColumn(HOSTNAME,         TXT("Host"),     m_hosts.StringWidth(20), LVCFMT_LEFT);
-	m_hosts.InsertColumn(LAST_BOOTUP_TIME, TXT("Rebooted"), m_hosts.StringWidth(20), LVCFMT_LEFT);
+	m_hostView.InsertColumn(HOST_NAME,        TXT("Host"),       m_hostView.StringWidth(20), LVCFMT_LEFT);
+	m_hostView.InsertColumn(TOTAL_MEMORY,     TXT("Total Mem"),  m_hostView.StringWidth(10), LVCFMT_LEFT);
+	m_hostView.InsertColumn(FREE_MEMORY,      TXT("Free Mem"),   m_hostView.StringWidth(10), LVCFMT_LEFT);
+	m_hostView.InsertColumn(DISK_USAGE,       TXT("C: Usage"),   m_hostView.StringWidth(10), LVCFMT_LEFT);
+	m_hostView.InsertColumn(LAST_BOOTUP_TIME, TXT("Rebooted"),   m_hostView.StringWidth(20), LVCFMT_LEFT);
+	m_hostView.InsertColumn(LAST_ERROR,       TXT("Last Error"), m_hostView.StringWidth(25), LVCFMT_LEFT);
 
-	for (size_t i = 0; i < hosts.size(); ++i)
-	{
-		const tstring& host = hosts[i];
+	for (size_t i = 0; i != m_hosts->size(); ++i)
+		addHostToView(i);
+}
 
-		m_hosts.InsertItem(i,                   host);
-		m_hosts.ItemText  (i, LAST_BOOTUP_TIME, TXT(""));
-	}
+////////////////////////////////////////////////////////////////////////////////
+//! Add a host to the view.
+
+void AppDlg::addHostToView(size_t index)
+{
+	m_hostView.InsertItem(index, m_hosts->name(index));
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//! Formats a memory size value. Memory sizes are KB based.
+
+tstring FormatMemoryValue(uint64 valueInKb)
+{
+	double valueInGb = valueInKb / (1024.0 * 1024.0);
+
+	return Core::fmt(TXT("%.1f GB"), valueInGb);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//! Formats the amount of disk space used.
+
+tstring FormatDiskUsage(const WMI::Win32_LogicalDisk& disk)
+{
+	uint64 diskSize = disk.Size();
+	uint64 freeSpace = disk.FreeSpace();
+	uint64 spaceUsed = diskSize - freeSpace;
+	double percentUsed = (spaceUsed * 100.0) / diskSize;
+
+	return Core::fmt(TXT("%.f %%"), percentUsed);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -67,25 +160,54 @@ void AppDlg::initialiseHostsView()
 
 void AppDlg::refreshView()
 {
-	const FarmMonitor::HostList& hosts = g_app.m_hostList;
+	for (size_t i = 0; i != m_hosts->size(); ++i)
+		refreshHost(i);
+}
 
-	WMI::Connection connection;
+////////////////////////////////////////////////////////////////////////////////
+//! Refresh the view for a single host.
 
-	for (size_t i = 0; i < hosts.size(); ++i)
+void AppDlg::refreshHost(size_t index)
+{
+	clearHost(index);
+
+	const tstring& host = m_hosts->name(index);
+
+	try
 	{
-		const tstring& host = hosts[i];
+		WMI::Connection connection(host);
 
-		try
+		WMI::Win32_OperatingSystem::Iterator osIter = WMI::Win32_OperatingSystem::select(connection);
+
+		m_hostView.ItemText(index, TOTAL_MEMORY,     FormatMemoryValue(osIter->TotalVirtualMemorySize()));
+		m_hostView.ItemText(index, FREE_MEMORY,      FormatMemoryValue(osIter->FreeVirtualMemory()));
+		m_hostView.ItemText(index, LAST_BOOTUP_TIME, osIter->LastBootUpTime().ToString());
+
+		WMI::Win32_LogicalDisk::Iterator diskIter = WMI::Win32_LogicalDisk::select(connection);
+		WMI::Win32_LogicalDisk::Iterator diskEnd;
+
+		for (; diskIter != diskEnd; ++diskIter)
 		{
-			connection.open(host);
-
-			WMI::Win32_OperatingSystem::Iterator it = WMI::Win32_OperatingSystem::select(connection);
-
-			m_hosts.ItemText(i, LAST_BOOTUP_TIME, it->LastBootUpTime().ToString());
-		}
-		catch (WMI::Exception& e)
-		{
-			TRACE2(TXT("%s: %s"), host.c_str(), e.twhat());
+			if (diskIter->DeviceID() == TXT("C:"))
+			{
+				m_hostView.ItemText(index, DISK_USAGE, FormatDiskUsage(*diskIter));
+			}
 		}
 	}
+	catch (WMI::Exception& e)
+	{
+		m_hostView.ItemText(index, LAST_ERROR, e.twhat());
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//! Clear the results for the host.
+
+void AppDlg::clearHost(size_t index)
+{
+	m_hostView.ItemText(index, TOTAL_MEMORY,     TXT(""));
+	m_hostView.ItemText(index, FREE_MEMORY,      TXT(""));
+	m_hostView.ItemText(index, DISK_USAGE,       TXT(""));
+	m_hostView.ItemText(index, LAST_BOOTUP_TIME, TXT(""));
+	m_hostView.ItemText(index, LAST_ERROR,       TXT(""));
 }
--- AppDlg.hpp
+++ AppDlg.hpp
@@ -13,6 +13,7 @@
 
 #include <WCL/MainDlg.hpp>
 #include <WCL/ListView.hpp>
+#include "Hosts.hpp"
 
 ////////////////////////////////////////////////////////////////////////////////
 //! The application main dialog. This is the dialog that sits in the centre of
@@ -22,16 +23,30 @@ class AppDlg : public CMainDlg
 {
 public:
 	//! Constructor.
-	AppDlg();
+	AppDlg(HostsPtr hosts);
 
 	//! Destructor.
 	virtual ~AppDlg();
+
+	//! Is a host currently selected?
+	bool isHostSelected() const;
+
+	//! Add a new host to be monitored.
+	void addHost(const tstring& hostname);
+	
+	//! Remove the currently selected host.
+	void removeSelectedHost();
 	
 private:
 	//
+	// Members.
+	//
+	HostsPtr	m_hosts;		//!< The hosts being monitored.
+
+	//
 	// Controls.
 	//
-	CListView	m_hosts;	//!< The main display.
+	CListView	m_hostView;		//!< The main display.
 
 	//
 	// Message processors.
@@ -40,21 +55,37 @@ private:
 	//! Handle dialog creation.
 	virtual void OnInitDialog();
 
+	//! Handle change in host selection.
+	LRESULT onHostSelectionChanged(NMHDR& header);
+
 	//
 	// Internal methods.
 	//
 
 	//! Initialise the hosts view.
-	void initialiseHostsView();
+	void initialiseHostView();
+
+	//! Add a host to the view.
+	void addHostToView(size_t index);
 
 	//! Refresh the hosts view.
 	void refreshView();
 
+	//! Refresh the view for a single host.
+	void refreshHost(size_t index);
+
+	//! Clear the results for the host.
+	void clearHost(size_t index);
+
 	//! The hosts view columns.
 	enum Column
 	{
-		HOSTNAME,
+		HOST_NAME,
+		TOTAL_MEMORY,
+		FREE_MEMORY,
+		DISK_USAGE,
 		LAST_BOOTUP_TIME,
+		LAST_ERROR,
 
 		NUM_COLUMNS,
 	};
