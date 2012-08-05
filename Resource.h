45745bf8338326c9822fbd904605b58aaa39c65a
Admin <admin@chrisoldwood.com>
2012-08-05 20:13:29 +0000

    Added basic menu and dialog to add a host. Inital icon.

--- Resource.h
+++ Resource.h
@@ -8,6 +8,10 @@
 #define IDR_APPTOOLBAR                  5
 #define ID_FILE_POPUP                   100
 #define ID_FILE_EXIT                    109
+#define IDD_ADD_HOST                    131
+#define ID_EDIT_POPUP                   200
+#define ID_EDIT_ADDHOST                 201
+#define ID_EDIT_REMOVEHOST              202
 #define ID_HELP_POPUP                   900
 #define ID_HELP_CONTENTS                901
 #define ID_HELP_ABOUT                   902
@@ -15,8 +19,9 @@
 #define IDC_EMAIL                       1084
 #define IDC_VERSION                     1085
 #define IDC_COPYRIGHT                   1086
-#define IDC_LIST1                       1086
 #define IDC_HOSTS                       1086
+#define IDC_EDIT1                       1090
+#define IDC_HOST_NAME                   1090
 #define IDD_MAIN                        5000
 #define IDD_ABOUT                       5001
 #define IDC_STATIC                      -1
@@ -26,9 +31,9 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        131
-#define _APS_NEXT_COMMAND_VALUE         163
-#define _APS_NEXT_CONTROL_VALUE         1087
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         165
+#define _APS_NEXT_CONTROL_VALUE         1091
 #define _APS_NEXT_SYMED_VALUE           104
 #endif
 #endif
